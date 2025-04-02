f69773043bc6734454c3740d05b4fb9a988d1573
Gabriel Derrico <derrico.g@aluno.ifsp.edu.br>
2025-04-02 16:33:50 -0300

    Ajustando estrutura de pastas

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+      <View className="flex-1 justify-center items-center">
+          <Text className="text-blue-400 text-xl">olá mundo</Text>
+      </View>
+       
+      
+
+  );
+}
---
+++ src/constants/images.js
@@ -0,0 +1,5 @@
+// Arquivo para importar as imagens de assets que serão usadas e exportar, para facilitar o uso em outros arquivos
+
+import logoParqueOnca from "../assets/images/logoParqueOnca.png";
+
+export default { logoOnca : logoParqueOnca };
