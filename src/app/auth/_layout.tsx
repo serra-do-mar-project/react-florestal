d107c2025dde563bbdb3039bbdca07905f9c5c40
EMANUEL DA COSTA ANDRADE CARDOSO <CG3028691@car.ifsp>
2025-04-22 21:21:59 -0300

    Tela Guia-fauna

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -2,23 +2,16 @@ import { Stack } from "expo-router";
 import { View, Image, Text } from "react-native";
 import Tab from "@/src/components/Tab";
 import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
 
 export default function AuthLayout() {
   return (
-
-      <>
-    
-        <Stack
-          screenOptions={{
-            headerShown: false, // Oculta o cabeçalho padrão
-            contentStyle: {
-              backgroundColor: "#F6F5F5",
-            },
-          }}
-        />
-
-        <Tab/>
-
-      </>
+    <>
+      <Stack>
+        <Stack.Screen name="searchPage" options={{headerShown: false}}/>
+        <Stack.Screen name="fauna" options={{headerShown: false}}/>
+      </Stack>
+      <Tab/>
+    </>
   );
 }
