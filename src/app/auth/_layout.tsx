d451e4c53b965662922bbb2a5e7b59dc8e946e17
Gabriel <141684784+gabderrico05@users.noreply.github.com>
2025-05-27 21:30:02 -0300

    Gabriel dev (#26)
    * Ajustando estrutura de pastas
    
    * feat: titulo, Logo do parque e input
    
    * feat: tela de login
    
    * feat: configurar tailwind para outros tamanhos de telas; flex nos brasões
    
    * configurando tailwind para diferentes tamanhos de tela; ajustando componentes;
    
    * Arquivos de  config tailwind, metro, global.css babel, modificados
    
    * AuthLayout + Tab com navegação
    
    * Montando Tela de pesquisa
    
    * Tela de Pesquisa finalizada: Cards
    funcionais e desing ajustado
    
    * adicionando o safeAreaView envolvendo o Stack do _layout, redefinido o tamanho da screen xs
    
    * initial
    
    * splash screen desing finalizada
    
    * splash screen retirada do index
    
    ---------
    
    Co-authored-by: Emanuel <107555186+EmanuelCAC@users.noreply.github.com>

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -1,7 +1,6 @@
 import { Stack } from "expo-router";
 import { View, Image, Text } from "react-native";
 import Tab from "@/src/components/Tab";
-
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 
--- src/constants/images.js
+++ src/constants/images.js
@@ -13,6 +13,11 @@ import flora from "@/src/assets/images/flora-img.png"
 import construcao from "@/src/assets/images/construcao-img.png"
 import usoPublico from "@/src/assets/images/uso-publico-img.png"
 import mineracao from "@/src/assets/images/mineracao-img.png"
+import splashbg from "@/src/assets/images/splashbg.png"
+import logoparque from "@/src/assets/images/logo-parque.svg"
+import fundacaoFlorestalBranca from "@/src/assets/images/fundacaoflorestal-branca.svg"
+import ifsplogo from "@/src/assets/images/ifsplogo.svg"
+import splogoBranca from "@/src/assets/images/logoSP-branca.svg"
 import bird from "@/src/assets/images/bird.svg"
 import leftArrow from "@/src/assets/images/left-arrow.svg"
 import ifspLogo from "@/src/assets/images/ifsp-logo.png"
@@ -37,6 +42,11 @@ const images = {
   construcao,
   mineracao,
   usoPublico,
+  splashbg,
+  logoparque,
+  fundacaoFlorestalBranca,
+  ifsplogo,
+  splogoBranca
   bird,
   leftArrow,
   ifspLogo,
