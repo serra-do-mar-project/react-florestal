6f83ce806d6fb91aa7571bf3623dd3727b2ad39b
Luiz Gustavo Alves dos Santos <149258158+L-Gustavo-Alves@users.noreply.github.com>
2025-06-29 16:28:44 -0300

    Dev luiz (#42)
    * Alteração da Splash-Screen,
    Criação do componente "Bottombar.tsx" para a bottombar da Splash-Screen,
    Alteração da Logo do Parque;
    
    * small alts on splash screen
    
    * Add files via upload
    
    * Update index.tsx
    
    ---------
    
    Co-authored-by: Luiz Gustavo Alves <122243200+Vonshrill@users.noreply.github.com>
    Co-authored-by: L-Gustavo-Alves <luiz.alves.1@outlook.com>

---
+++ src/components/Bottombar.tsx
@@ -0,0 +1,18 @@
+import { View } from "react-native";
+import images from "../constants/images";
+
+export default function BottomBar() {
+  return (
+    <View className="w-full bg-[#3E4B2E] py-4 px-6 flex-row justify-around items-center">
+      <View className="items-center justify-center">
+        <images.ifsplogoBranca width={100} height={60} style={{ resizeMode: "contain" }} />
+      </View>
+      <View className="items-center justify-center">
+        <images.fundacaoFlorestalBranca width={100} height={60} style={{ resizeMode: "contain" }} />
+      </View>
+      <View className="items-center justify-center">
+        <images.splogoBranca width={100} height={60} style={{ resizeMode: "contain" }} />
+      </View>
+    </View>
+  );
+}
