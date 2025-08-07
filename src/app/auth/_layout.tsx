4c780a149bb0a57e2856ed8a77d3620cc74539eb
EmanuelCAC <emanuelc279@gmail.com>
2025-08-07 20:17:39 -0300

    fix white space bellow the footer on the splash screen and tab bar indication

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -51,9 +51,10 @@ const TabsLayout = () => {
         }}
       >
         <Tabs.Screen
-          name="search/index" 
+          name="search" 
           options={{
             title: "Pesquisar",
+            href: "/auth/search",
             headerShown: false,
             tabBarIcon: ({focused}) => (
               <TabIcon 
@@ -95,12 +96,12 @@ const TabsLayout = () => {
             )
           }}  
         />
-        <Tabs.Screen name="search/fauna" options={{ href: null, headerShown: false, }} />
+        {/* <Tabs.Screen name="search/fauna" options={{ href: null, headerShown: false, }} />
         <Tabs.Screen name="search/flora" options={{ href: null, headerShown: false, }} />
         <Tabs.Screen name="search/procedimentos" options={{ href: null, headerShown: false, }} />
         <Tabs.Screen name="search/mineracao" options={{ href: null, headerShown: false, }} />
         <Tabs.Screen name="search/usoPublico" options={{ href: null, headerShown: false, }} />
-        <Tabs.Screen name="search/construcao" options={{ href: null, headerShown: false, }} />
+        <Tabs.Screen name="search/construcao" options={{ href: null, headerShown: false, }} /> */}
       </Tabs>
     </>
   )
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { View, Image, Text, SafeAreaView, Dimensions } from "react-native";
+import { View, Image, Text, Dimensions } from "react-native";
 import images from "../constants/images";
 import { SubmitButton } from "../components/SubmitButton";
 import { useRouter } from "expo-router";
 import BottomBar from "../components/Bottombar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const { height } = Dimensions.get("window");
 
@@ -12,21 +13,16 @@ export default function Index() {
   const router = useRouter();
 
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="flex-1 items-center z-10">
-        
-        <View className="mt-6">
+    <SafeAreaView className="flex-1 bg-white items-center">
+        <View className="mt-6 px-5 items-center gap-3 flex-[1/3]">
           <images.logoparque width={300} height={150} />
-        </View>
-
-        <View className="mt-3 px-5">
           <Text className="text-center text-2xl font-bold text-black leading-7">
             MANUAL DE PROCEDIMENTOS{"\n"}
             OPERACIONAIS E ADMINISTRATIVOS
           </Text>
         </View>
 
-        <View className="mt-2 w-full h-[60%] relative">
+        <View className="mt-2 w-full flex-1 relative mb-auto">
           <Image
             source={images.splashbg}
             className="w-full h-full"
@@ -36,13 +32,12 @@ export default function Index() {
           <View className="absolute bottom-24 w-full items-center">
             <SubmitButton
               title="ENTRAR"
-              onPress={() => router.push("/loginPage")}
+              onPress={() => router.push("/loginPage")}   
             />
           </View>
         </View>
 
         <BottomBar />
-      </View>
     </SafeAreaView>
   );
 }
