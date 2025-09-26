ded33b052f7e1a84adf0ced34dfd4cd7e7cf020e
Gabriel Derrico <derrico.g@aluno.ifsp.edu.br>
2025-09-26 15:42:12 -0300

    feat: generate components list from autos

--- src/app/auth/search/procedimentos.tsx
+++ src/app/auth/search/procedimentos.tsx
@@ -6,6 +6,112 @@ import React, { useEffect, useState } from "react";
 import db from "@/src/db/connection";
 import { infracoesTable, Infracao } from "@/src/db/schema";
 import { eq } from "drizzle-orm";
+import FormCard from "@/src/components/report/FormCard";
+
+const mockData = {
+      "infractionType": "Reforma sem autorização",
+      "infractionLaw": "Art. 67.da Resolução SIMA N° 05/2021 Realizar quaisquer atividades ou adotar conduta em desacordo com os objetivos da unidade de conservação, o seu plano de manejo e regulamentos: Multa de R$ 500,00 (quinhentos reais), majorada até R$ 10.000,00 (dez mil reais), mediante laudo técnico do órgão gestor da unidade de conservação.",
+      "components": [
+        {
+          "name": "Fiscalização",
+          "Fiscalização": {
+            "type": "dropdown",
+            "dropdown": ["Rotina", "Integrada ao pelotão", "Integrada a companhia", "Integrada ao batalhão"],
+            "id": 0
+          }
+        },
+        {
+          "name": "Local",
+          "Local": [
+            {
+              "type": "string",
+              "name": "Estrada",
+              "id": 1
+            },
+            {
+              "type": "string",
+              "name": "Bairro",
+              "id": 2
+            },
+            {
+              "type": "string",
+              "name": "Setor",
+              "id": 3
+            },
+            {
+              "type": "string",
+              "name": "Primeira estrutura",
+              "id": 6
+            },
+            {
+              "type": "string",
+              "name": "Latitude da primeira estrutura",
+              "id": 7
+            },
+            {
+              "type": "string",
+              "name": "Longitude da primeira estrutura",
+              "id": 8
+            },
+            {
+              "type": "string",
+              "name": "Segunda estrutura",
+              "id": 9
+            },
+            {
+              "type": "string",
+              "name": "Latitude da segunda estrutura",
+              "id": 10
+            },
+            {
+              "type": "string",
+              "name": "Longitude da segunda estrutura",
+              "id": 11
+            },
+            {
+              "type": "string",
+              "name": "Longitude da unidade de conservação",
+              "id": 13
+            },
+            {
+              "type": "string",
+              "name": "Latitude da unidade de conservação",
+              "id": 14
+            }
+          ]
+        },
+        {
+          "name": "Horario",
+          "Horario": {
+            "type": "date",
+            "name": "Horário da infração",
+            "id": 4
+          }
+        },
+        {
+          "name": "Informações Extras",
+          "Informacoes Extras": [
+            {
+              "type": "string",
+              "name": "Descrição da intervenção",
+              "id": 12
+            }
+          ]
+        },
+        {
+          "name": "Dados do Infrator",
+          "Dados do Infrator": [
+            {
+              "type": "string",
+              "name": "Nome do infrator",
+              "id": 5
+            }
+          ]
+        }
+      ]
+    }
+  
+
 
 export default function ProcedimentosPage() {
   const router = useRouter();
@@ -89,6 +195,24 @@ export default function ProcedimentosPage() {
               }
             </View>
           </View>
+
+          <View className="w-full pt-10 items-center px-4">
+
+              {(mockData.components ?? "")
+                .toString()
+                .split(",")
+                .map((component, idx) =>
+                    (
+                    <FormCard key={idx}>
+                      <Text>{component.trim()}</Text>
+                    </FormCard>
+                  )
+                )
+              }
+
+
+          </View>
+          
 
           <SubmitButton
             classname="h-[3rem] w-[9rem] mt-9 mb-10"
