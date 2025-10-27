ffc5388b056a534c73cfe3d79c9102a610990500
Gabriel Derrico <derrico.g@aluno.ifsp.edu.br>
2025-10-27 14:31:29 -0300

    refactor(connection): coment code for clear local db in dev mode

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,15 +1,16 @@
 import * as SQLite from "expo-sqlite";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 
+// Descomente esse código caso queira limpar o banco local
 
-if (__DEV__) {
-  try {
-    SQLite.deleteDatabaseAsync("db.db");
-    console.log("✅ Banco de dados deletado (modo dev)");
-  } catch (e) {
-    console.error("Erro ao deletar o banco:", e);
-  }
-}
+// if (__DEV__) {
+//   try {
+//     SQLite.deleteDatabaseAsync("db.db");
+//     console.log("✅ Banco de dados deletado (modo dev)");
+//   } catch (e) {
+//     console.error("Erro ao deletar o banco:", e);
+//   }
+// }
 
 
 const expo = SQLite.openDatabaseSync("db.db");
