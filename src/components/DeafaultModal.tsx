57dee88efd604a61d454d5e03c975275614a849c
Gabriel Derrico <derrico.g@aluno.ifsp.edu.br>
2025-11-10 02:32:53 -0300

    refactor(defaultModal): fix touch issue on children by using native animations for modal transitions

--- src/components/DeafaultModal.tsx
+++ src/components/DeafaultModal.tsx
@@ -1,15 +1,17 @@
-
-import { createContext, useContext, useEffect, useState } from "react";
-import { Modal, ModalProps, Pressable, View, Dimensions, ScrollView, Keyboard } from "react-native";
+import { createContext, useContext, useEffect,} from "react";
+import { Modal, ModalProps, Pressable, View, Dimensions, ScrollView } from "react-native";
 import { Gesture, GestureDetector, GestureHandlerRootView,  } from "react-native-gesture-handler";
-import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming, useAnimatedReaction } from "react-native-reanimated";
+import { PasswordInput } from "./PasswordInput";
 import React from "react";
-
+import { cn } from "../lib/utils";
 
 export type DefaultModalProps = ModalProps & {
   visible: boolean;
   onClose: () => void;
   children?: React.ReactNode;
+  beforeClose?: () => void;
+  scrollClassName?: string;
 }
 
 type ModalContextType = {
@@ -24,42 +26,36 @@ export const useModal = () => {
   return context;
 };
 
-export function DefaultModal({visible, onClose, children, ...rest}: DefaultModalProps) {
+export function DefaultModal({visible, onClose, children, beforeClose, scrollClassName, ...rest}: DefaultModalProps) {
 
   const translateY = useSharedValue(0);
-  const overlayOpacity = useSharedValue(0);
 
   const SCREEN_HEIGHT = Dimensions.get('window').height;
-  const [keyboardVisible, setKeyboardVisible] = useState(false);
   
-  useEffect(() => {
-    const showSub = Keyboard.addListener('keyboardDidShow', () => setKeyboardVisible(true));
-    const hideSub = Keyboard.addListener('keyboardDidHide', () => setKeyboardVisible(false));
-    return () => {
-      showSub.remove();
-      hideSub.remove();
-    };
-  }, []);
 
   useEffect(() => {
     translateY.value = SCREEN_HEIGHT;
-    overlayOpacity.value = 0;
-  }, [SCREEN_HEIGHT, translateY, overlayOpacity]);
+  }, [SCREEN_HEIGHT, translateY, ]);
 
 
   useEffect(() => {
     if (visible) {
-      overlayOpacity.value = withTiming(0.5, { duration: 180 });
       translateY.value = withTiming(0, { duration: 250 });
     }
-  }, [visible, overlayOpacity, translateY]);
+  }, [visible, , translateY]);
 
-  function closeWithAnimation() {
-    overlayOpacity.value = withTiming(0, { duration: 150 });
-    translateY.value = withTiming(SCREEN_HEIGHT, { duration: 250 }, (finished) => {
-      if (finished) runOnJS(onClose)();
-    });
-  }
+
+
+  const closeWithAnimation = () => {
+  beforeClose && beforeClose();
+
+  translateY.value = withTiming(SCREEN_HEIGHT, { duration: 250 }, (finished) => {
+    if (finished) {
+
+    runOnJS(onClose)();
+    }
+  });
+};
 
   const panGesture = Gesture.Pan()
   .onUpdate(e => {
@@ -67,14 +63,12 @@ export function DefaultModal({visible, onClose, children, ...rest}: DefaultModal
   })
   .onEnd(() => {
     if (translateY.value > 120) {
-      overlayOpacity.value = withTiming(0, { duration: 150 });
       translateY.value = withTiming(SCREEN_HEIGHT, { duration: 250 }, (finished) => {
         if (finished) runOnJS(onClose)();
       });
     } else {
       // volta
-      overlayOpacity.value = withTiming(0.5, { duration: 150 });
-      translateY.value = withTiming(0, { duration: 150 });
+     translateY.value = withTiming(0, { duration: 150 });
     }
   });
 
@@ -85,35 +79,39 @@ export function DefaultModal({visible, onClose, children, ...rest}: DefaultModal
 
   return(
     // desativa animação nativa e animamos nós mesmos
-    <Modal className="flex-" statusBarTranslucent visible={visible} transparent animationType="none" onRequestClose={closeWithAnimation} {...rest} >
-      <ModalContext.Provider value={{ closeWithAnimation }}>
-      <GestureHandlerRootView style={{ flex: 1 }} >
-        <View className="flex-1 bg-black/50" pointerEvents="box-none">
-          <Pressable className="flex-1" onPress={closeWithAnimation}  pointerEvents="box-only"/>
+    <Modal className="flex-1" statusBarTranslucent visible={visible} transparent animationType="none" onRequestClose={closeWithAnimation} {...rest} >
+      
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View className="flex-1 bg-black/50">
+          <Pressable className="flex-1 justify-end" onPress={closeWithAnimation} pointerEvents="box-none"/>
           
+
             <Animated.View
-              className={`w-full min-h-1/3 absolute bottom-0 ${keyboardVisible && "top-safe"} max-h-[${SCREEN_HEIGHT}] items-center bg-white z-10`}
-              style={[animatedStyle, { borderTopLeftRadius: 24, borderTopRightRadius: 24, overflow: 'hidden'}]}
+              pointerEvents="box-none"
+              className={`w-full min-h-1/3 max-h-[${SCREEN_HEIGHT}] items-center bg-white mt-10`}
+              style={[animatedStyle, { borderTopLeftRadius: 24, borderTopRightRadius: 24,}]}
             >
               <GestureDetector gesture={panGesture}>
-              <Pressable className="w-full h-14" onPress={closeWithAnimation}>
-              <View
-                className="h-1.5 w-16 bg-gray-400/50 rounded-full mx-auto mt-6"
-                
-              />
-              </Pressable>
-              </GestureDetector>
-                <ScrollView className="w-full h-full z-20" keyboardShouldPersistTaps="handled" showsVerticalScrollIndicator={false}>
 
-                  {children}
-                </ScrollView>
-                 
+              <Pressable className="w-full h-14" onPress={closeWithAnimation}>
+              <View className="h-1.5 w-16 bg-gray-400/50 rounded-full mx-auto mt-6"/>
+              </Pressable>
+
+              </GestureDetector>
+
+                <ModalContext.Provider value={{ closeWithAnimation }} >
+                    <View className="w-full" pointerEvents="auto">
+                        {children}
+                    </View>
+                </ModalContext.Provider>
             </Animated.View>
-        
+            
         </View>
       </GestureHandlerRootView>
-      </ModalContext.Provider>
+      
     </Modal>
  
    );
  }
+
+    
--- src/components/DefaultModal.tsx
+++ src/components/DefaultModal.tsx
@@ -1,10 +1,14 @@
-import { createContext, useContext, useEffect,} from "react";
-import { Modal, ModalProps, Pressable, View, Dimensions, ScrollView } from "react-native";
-import { Gesture, GestureDetector, GestureHandlerRootView,  } from "react-native-gesture-handler";
-import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
-import { PasswordInput } from "./PasswordInput";
-import React from "react";
-import { cn } from "../lib/utils";
+import React, { createContext, useContext, useEffect, useRef } from "react";
+import {
+  Modal,
+  ModalProps,
+  Pressable,
+  View,
+  Dimensions,
+  Animated, // 👈 nativo
+  PanResponder,
+} from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export type DefaultModalProps = ModalProps & {
   visible: boolean;
@@ -12,7 +16,7 @@ export type DefaultModalProps = ModalProps & {
   children?: React.ReactNode;
   beforeClose?: () => void;
   scrollClassName?: string;
-}
+};
 
 type ModalContextType = {
   closeWithAnimation: () => void;
@@ -22,93 +26,129 @@ const ModalContext = createContext<ModalContextType | undefined>(undefined);
 
 export const useModal = () => {
   const context = useContext(ModalContext);
-  if (!context) throw new Error("useModal deve ser usado dentro de <DefaultModal>");
+  if (!context)
+    throw new Error("useModal deve ser usado dentro de <DefaultModal>");
   return context;
 };
 
-export function DefaultModal({visible, onClose, children, beforeClose, scrollClassName, ...rest}: DefaultModalProps) {
+export function DefaultModal({
+  visible,
+  onClose,
+  children,
+  beforeClose,
+  ...rest
+}: DefaultModalProps) {
+  const SCREEN_HEIGHT = Dimensions.get("window").height;
+  const translateY = useRef(new Animated.Value(SCREEN_HEIGHT)).current;
 
-  const translateY = useSharedValue(0);
-  const overlayOpacity = useSharedValue(0);
-
-  const SCREEN_HEIGHT = Dimensions.get('window').height;
-  
-
-  useEffect(() => {
-    translateY.value = SCREEN_HEIGHT;
-    overlayOpacity.value = 0;
-  }, [SCREEN_HEIGHT, translateY, overlayOpacity]);
-
-
+  // ✅ Abre e fecha com animação
   useEffect(() => {
     if (visible) {
-      overlayOpacity.value = withTiming(0.5, { duration: 180 });
-      translateY.value = withTiming(0, { duration: 250 });
-    }
-  }, [visible, overlayOpacity, translateY]);
-
-  
-
-  function closeWithAnimation() {
-    beforeClose&& beforeClose();
-    overlayOpacity.value = withTiming(0, { duration: 150 });
-    translateY.value = withTiming(SCREEN_HEIGHT, { duration: 250 }, (finished) => {
-      if (finished) runOnJS(onClose)();
-    });
-  }
-
-  const panGesture = Gesture.Pan()
-  .onUpdate(e => {
-    if (e.translationY > 0) translateY.value = e.translationY;
-  })
-  .onEnd(() => {
-    if (translateY.value > 120) {
-      overlayOpacity.value = withTiming(0, { duration: 150 });
-      translateY.value = withTiming(SCREEN_HEIGHT, { duration: 250 }, (finished) => {
-        if (finished) runOnJS(onClose)();
-      });
+      Animated.spring(translateY, {
+        toValue: 0,
+        useNativeDriver: true,
+        bounciness: 0,
+      }).start();
     } else {
-      // volta
-      overlayOpacity.value = withTiming(0.5, { duration: 150 });
-      translateY.value = withTiming(0, { duration: 150 });
+      Animated.timing(translateY, {
+        toValue: SCREEN_HEIGHT,
+        duration: 250,
+        useNativeDriver: true,
+      }).start();
     }
-  });
+  }, [visible]);
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ translateY: translateY.value }],
-  }));
+  const closeWithAnimation = () => {
+    beforeClose && beforeClose();
+    Animated.timing(translateY, {
+      toValue: SCREEN_HEIGHT,
+      duration: 250,
+      useNativeDriver: true,
+    }).start(({ finished }) => {
+      if (finished) onClose();
+    });
+  };
 
+  // ✅ Usa PanResponder em vez de GestureDetector (sem Reanimated)
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: (_, gestureState) =>
+        gestureState.dy > 10, // só ativa se arrastar pra baixo
+      onPanResponderMove: (_, gestureState) => {
+        if (gestureState.dy > 0) translateY.setValue(gestureState.dy);
+      },
+      onPanResponderRelease: (_, gestureState) => {
+        if (gestureState.dy > 120) {
+          Animated.timing(translateY, {
+            toValue: SCREEN_HEIGHT,
+            duration: 250,
+            useNativeDriver: true,
+          }).start(({ finished }) => {
+            if (finished) onClose();
+          });
+        } else {
+          Animated.spring(translateY, {
+            toValue: 0,
+            useNativeDriver: true,
+          }).start();
+        }
+      },
+    })
+  ).current;
 
-  return(
-    // desativa animação nativa e animamos nós mesmos
-    <Modal className="flex-1" statusBarTranslucent visible={visible} transparent animationType="none" onRequestClose={closeWithAnimation} {...rest} >
-      
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="none"
+      statusBarTranslucent
+      onRequestClose={closeWithAnimation}
+      {...rest}
+    >
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <View className="flex-1 bg-black/50" pointerEvents="box-none">
-          <Pressable className="flex-1" onPress={closeWithAnimation}  pointerEvents="box-only"/>
-          <ModalContext.Provider value={{ closeWithAnimation }} >
-
-            <Animated.View
-              className={`w-full min-h-1/3 max-h-[${SCREEN_HEIGHT}] items-center bg-white mt-10`}
-              style={[animatedStyle, { borderTopLeftRadius: 24, borderTopRightRadius: 24, overflow: 'hidden',  zIndex: 100}]}
+        <View style={{ flex: 1, backgroundColor: "rgba(0,0,0,0.5)" }}>
+          <Pressable
+            style={{ flex: 1, justifyContent: "flex-end" }}
+            onPress={closeWithAnimation}
+            pointerEvents="box-none"
+          />
+          <Animated.View
+            {...panResponder.panHandlers} // 👈 aplica gesto
+            style={{
+              transform: [{ translateY }],
+              borderTopLeftRadius: 24,
+              borderTopRightRadius: 24,
+              marginTop: 30,
+              backgroundColor: "white",
+              width: "100%",
+              minHeight: "33%",
+              maxHeight: "95%",
+            }}
+          >
+            <Pressable
+              style={{ width: "100%", height: 50 }}
+              onPress={closeWithAnimation}
             >
-              <GestureDetector gesture={panGesture}>
-              <Pressable className="w-full h-14" onPress={closeWithAnimation}>
               <View
-                className="h-1.5 w-16 bg-gray-400/50 rounded-full mx-auto mt-6"
-                
+                style={{
+                  height: 6,
+                  width: 60,
+                  backgroundColor: "#ccc",
+                  borderRadius: 3,
+                  alignSelf: "center",
+                  marginTop: 10,
+                }}
               />
-              </Pressable>
-              </GestureDetector>
-                  
-                  {children}
-                 
-            </Animated.View>
+            </Pressable>
+
+            <ModalContext.Provider value={{ closeWithAnimation }}>
+             
+                {children}
+              
             </ModalContext.Provider>
+          </Animated.View>
         </View>
       </GestureHandlerRootView>
-      
     </Modal>
- 
-   );
- }
+  );
+}
